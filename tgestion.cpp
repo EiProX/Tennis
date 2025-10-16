6947cb773b25797e06d5d6be2aa8fcd7efc216ed
EiProX <108833428+EiProX@users.noreply.github.com>
2025-10-16 09:46:30 +0200

    Update tgestion.cpp

--- tgestion.cpp
+++ tgestion.cpp
@@ -27,6 +27,8 @@ tGestion::~tGestion()
     delete m_rs232;
 }
 
+
+
 void tGestion::Tempo(int secondes)
 {
     sleep(secondes);
@@ -40,7 +42,23 @@ void tGestion::run()
     {
         char numero[50] = {0};
         int codeSecret = 0;
+        int verif =0;
+        sMembre lu{};
 
+        int indexALire = 0;
+        cout << "Lecture de l’enregistrement d’index " << indexALire << "..." << endl;
+        if (m_comptes->lire_membre(indexALire, &lu))
+         {
+            cout << "Lecture réussie :" << endl;
+            cout << "  Numéro : " << lu.numero << endl;
+            cout << "  Code   : " << lu.code << endl;
+            cout << "  Nom    : " << lu.nom << endl;
+        }
+        else
+        {
+            cout << "Erreur : impossible de lire l’enregistrement " << indexALire << endl;
+        }
+        cout << endl;
         // Lecture carte
         m_lecteur->LireNumero(numero);
         cout << "Carte détectée : " << numero << endl;
@@ -48,9 +66,13 @@ void tGestion::run()
         // Lecture code secret
         cout << "Entrez le code : ";
         cin >> codeSecret;
+        cout << codeSecret << " : Code rentré"<<endl;
 
+
+        verif = m_comptes->verifier(numero, codeSecret);
+        cout <<"Verifier = "<< verif<<endl;
         // Vérification
-        if (m_comptes->verifier(numero, codeSecret))
+        if (verif == 1)
         {
             cout << "Accès autorisé." << endl;
             m_materiel->DebloquerPorte();
